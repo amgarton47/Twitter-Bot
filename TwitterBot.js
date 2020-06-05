cec08a1b9d9a08ed1ec2be39efbb80b5bef29e47
Aidan Garton <amgb2018@mymail.pomona.edu>
2020-06-04 21:07:55 -0500

    Added media post functionality

--- TwitterBot.js
+++ TwitterBot.js
@@ -1,6 +1,9 @@
 const Twit = require("twit");
 const config = require("./config");
 const T = new Twit(config);
+const fs = require("fs");
+
+const axios = require("axios");
 
 // this function will post a tweet with 'tweetContent' as its text
 function postTweet(tweetContent) {
@@ -15,4 +18,45 @@ function postTweet(tweetContent) {
   });
 }
 
-postTweet("This is a Twitter Bot!");
+// post a tweet with media
+function postMediaTweet(caption) {
+  var b64content = fs.readFileSync(
+    "/Users/aidangarton/Desktop/Code/TwitterBot/Robots-Square.jpg",
+    { encoding: "base64" }
+  );
+
+  // first we must post the media to Twitter
+  T.post(
+    "media/upload",
+    {
+      media_data: b64content,
+    },
+    function (err, data, response) {
+      // now we can assign alt text to the media, for use by screen readers and
+      // other text-based presentations and interpreters
+      var mediaIdStr = data.media_id_string;
+      var altText =
+        "Small flowers in a planter on a sunny balcony, blossoming.";
+      var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+
+      T.post("media/metadata/create", meta_params, function (
+        err,
+        data,
+        response
+      ) {
+        if (!err) {
+          let tweetCaption = caption || "loving life #nofilter";
+          // now we can reference the media and post a tweet (media will attach to the tweet)
+          var params = {
+            status: tweetCaption,
+            media_ids: [mediaIdStr],
+          };
+
+          T.post("statuses/update", params, function (err, data, response) {
+            console.log("MEDIA POST SUCCESFUL");
+          });
+        }
+      });
+    }
+  );
+}
